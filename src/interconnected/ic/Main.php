51360c4154ed19f269512540782dd5c81b4671f6
notfenixio <103071021+NotFenixio@users.noreply.github.com>
2023-05-10 20:16:36 +0200

    Skeleton uncompleted

--- src/interconnected/ic/Main.php
+++ src/interconnected/ic/Main.php
@@ -1,4 +1,21 @@
 <?php
 
+namespace interconnected\ic;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine
+
+class Main extends PluginBaseBase implements Listener {
+    public function onEnable() {
+        $this->getLogger()->("Enabled Interconnected v1.");
+    }
+
+    public function onChat(PlayerChatEvent $event) {
+
+    }
+
+}
 
 ?>
